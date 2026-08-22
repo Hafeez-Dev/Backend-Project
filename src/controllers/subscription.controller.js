aaefdcd01fb62ed0bec3cdc5109196e3b2146dc9
Abdul Hafeez <debugerxy@gmail.com>
2026-08-22 15:56:28 +0500

    add subscription controller | update subscription model

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -9,16 +9,120 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 const toggleSubscription = asyncHandler(async (req, res) => {
     const {channelId} = req.params
     // TODO: toggle subscription
+    if(!isValidObjectId(channelId)) {
+        throw new ApiError(400, "Invalid channelId!")
+    }
+
+    const existedSubscriber = await Subscription.findOne({
+        channel: channelId,
+        subscriber: req.user._id
+    })
+
+    let subscribed
+    let message
+
+    if(!existedSubscriber) {
+        await Subscription.create({
+            subscriber: req.user._id,
+            channel: channelId
+        })
+
+        subscribed = true
+        message = "Subscribed to channel successfully!"
+
+    } else {
+        await existedSubscriber.deleteOne()
+
+        subscribed = false
+        message = "Unsubscribed from channel successfully!"
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, {subscribed}, message )
+    )
+
 })
 
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
     const {channelId} = req.params
+
+    if(!isValidObjectId(channelId)) {
+        throw new ApiError(400, "Invalid channelId!")
+    }
+
+    const subscribers = await Subscription.aggregate([
+        {
+            $match: { channel: new mongoose.Types.ObjectId(channelId) }
+        },
+        {
+            $lookup: {
+                from: "users",
+                localField: "subscriber",
+                foreignField: "_id",
+                as: "subscriber"
+            }
+        },
+        {
+            $unwind: "$subscriber"
+        },
+        {
+            $project: {
+                _id: "$subscriber._id",
+                username: "$subscriber.username",
+                fullName: "$subscriber.fullName",
+                avatar: "$subscriber.avatar.url"
+            }
+        }
+    ])
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, subscribers, "subscribers fetched successfully!")
+    )
 })
 
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
     const { subscriberId } = req.params
+
+    if(!isValidObjectId(subscriberId)) {
+        throw new ApiError(400, "Invalid subscriberId!")
+    }
+
+    const subscribedToChannels = await Subscription.aggregate([
+        {
+            $match: { subscriber: new mongoose.Types.ObjectId(subscriberId) }
+        },
+        {
+            $lookup: {
+                from: "users",
+                localField: "channel",
+                foreignField: "_id",
+                as: "channel"
+            }
+        },
+        {
+            $unwind: "$channel"
+        },
+        {
+            $project: {
+                _id: "$channel._id",
+                username: "$channel.username",
+                fullName: "$channel.fullName",
+                avatar: "$channel.avatar.url"
+            }
+        }
+    ])
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, subscribedToChannels, "subscribedTo channels are fetched successfully!")
+    )
 })
 
 export {
--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -3,12 +3,23 @@ import mongoose, { Schema } from "mongoose";
 const subscriptionSchema = new Schema({
     subscriber: {
         type: Schema.Types.ObjectId,
-        ref: "User"
+        ref: "User",
+        required: true
     },
     channel: {
         type: Schema.Types.ObjectId,
-        ref: "User"
+        ref: "User",
+        required: true
     }
 }, {timestamps: true})
+
+// One unique relationship per subscriber/channel pair
+subscriptionSchema.index(
+    { subscriber: 1, channel: 1 },
+    { unique: true }
+)
+
+// Quickly find everyone subscribed to a channel
+subscriptionSchema.index({ channel: 1 })
 
 export const Subscription = mongoose.model("Subscription", subscriptionSchema)
